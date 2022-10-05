dfc385c1160fdb7897ede1e761920c33621fcec7
David Robins <dbrobins@i4031.net>
2022-10-05 18:29:04 -0400

    Update for current locking/fetch plugin directory.

--- xbmc/filesystem/PluginDirectory.cpp
+++ xbmc/filesystem/PluginDirectory.cpp
@@ -556,8 +556,8 @@ bool CPluginDirectory::Resolve(CFileItem& item) const
 
 int CPluginDirectory::GetWatchedMode(int handle, const char *content)
 {
-  CSingleLock lock(m_handleLock);
-  CPluginDirectory *dir = dirFromHandle(handle);
+  std::unique_lock<CCriticalSection> lock(GetScriptsLock());
+  CPluginDirectory *dir = GetScriptFromHandle(handle);
   if (!dir)
     return WatchedModeAll;
 	return CMediaSettings::GetInstance().GetWatchedMode((!content || !content[0]) ? dir->m_listItems->GetContent() : content);
